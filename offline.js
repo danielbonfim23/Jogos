035170036bf888e628a7443a3c69718ef2844b58
danielbonfim23 <158781988+danielbonfim23@users.noreply.github.com>
2024-02-03 19:17:27 -0300

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,23 @@
+﻿{
+	"version": 1706614278,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/metal-sheet0.png",
+		"images/sprite-sheet0.png",
+		"images/botao_ligado_desligado-sheet0.png",
+		"images/botao_ligado_desligado-sheet1.png",
+		"images/lentes-sheet0.png",
+		"images/laser-sheet0.png",
+		"images/fundo-sheet0.png",
+		"images/sprite2-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
